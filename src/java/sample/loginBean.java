c4180f35ba016fe1ad120d2957e3862b9f5686cb
'MadhuriShah' <madhurishah393@gmail.com>
2015-04-17 12:30:14 -0400

    Added loginBean class

---
+++ src/java/sample/loginBean.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sample;
+
+/**
+ *
+ * @author c0647610
+ */
+public class loginBean {
+    private String username;
+    private String password;
+    private boolean isLoggedIn;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isIsLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setIsLoggedIn(boolean isLoggedIn) {
+        this.isLoggedIn = isLoggedIn;
+    }
+    public void doLogin(){
+        isLoggedIn=username.equals("Madhuri") && password.equals("shah");
+    }
+}
