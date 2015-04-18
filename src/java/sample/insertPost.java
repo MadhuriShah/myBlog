698040dc58287ae7734b6acb4824417d4c0f0342
'MadhuriShah' <madhurishah393@gmail.com>
2015-04-17 23:53:12 -0400

    added getResult  method, modified getAll

--- src/java/sample/insertPost.java
+++ src/java/sample/insertPost.java
@@ -5,20 +5,26 @@
  */
 package sample;
 
+import connection.myConnection;
 import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -28,8 +34,9 @@ import javax.ws.rs.QueryParam;
 public class insertPost {
 
     @GET
-    public String getAll() {
-        return "Hello World";
+    @Produces("application/json")
+    public Response getAll() {
+        return Response.ok(getResults("select * from post")).build();
     }
 
     @POST
@@ -55,5 +62,30 @@ public class insertPost {
             Logger.getLogger(insertPost.class.getName()).log(Level.SEVERE, null, ex);
         }
         return changes;
+    }
+    
+       public static JsonArray getResults(String sql, String... params) {
+        JsonArray json = null;
+        try {
+            JsonArrayBuilder array = Json.createArrayBuilder();
+            Connection conn = connection.myConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            for (int i = 0; i < params.length; i++)
+                pstmt.setString(i+1, params[i]);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                array.add(Json.createObjectBuilder()
+                    .add("p_id", rs.getInt("p_id"))
+                    .add("title", rs.getString("title"))
+                    .add("description", rs.getString("description"))
+                    .add("c_id", rs.getInt("c_id"))
+                    .build());
+            }
+            conn.close();
+            json = array.build();
+        } catch (SQLException ex) {
+            Logger.getLogger(insertPost.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return json;
     }
 }
